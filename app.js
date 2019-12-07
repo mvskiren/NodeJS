af4f0ae6d5e8529394f3682d84c6cbb7478d9b34
mvskiren <kiranmvs07@gmail.com>
2019-12-08 00:20:33 +0530

    Done with Lodash npm external module import and using and nodemon for contionous montioring

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-
 const fs=require('fs');
 const os=require('os');
+const _=require('lodash');
+const user=os.userInfo();
+const note=require('./noteapp.js');
 console.log("hello");
-fs.appendFileSync('greetings.txt', 'helloworld!');
+console.log(_.isString("Kiran"));
+fs.appendFileSync('greetings.txt', `Hello ${user.username}! Your are ${note.add(3,5)}.`);
---
+++ noteapp.js
@@ -0,0 +1,4 @@
+module.exports.age='25';
+module.exports.add= (a,b) =>{
+   return a*b;
+};
