b4b5fbf6ea6bf2c50ab805fc595af34a94c76511
mvskiren <kiranmvs07@gmail.com>
2019-12-09 19:53:55 +0530

    node --inspect-brk playground/debugging.js

---
+++ Playground/debugging.js
@@ -0,0 +1,10 @@
+var person ={
+    name: 'kiran',
+    age: 24
+};
+debugger;
+person.name="sai";
+
+ // node --inspect-brk playground/debugging.js command to run to inspect in chrome browser 
+ //  chrome://inspect 
+ open dev dedicated tool
