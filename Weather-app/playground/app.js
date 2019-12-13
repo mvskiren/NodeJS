eada29dc7ad5cca6cdcbd7c14b19a53424d62bc7
mvskiren <kiranmvs07@gmail.com>
2019-12-14 00:31:57 +0530

    Updated link and comments

--- Weather-app/playground/app.js
+++ Weather-app/playground/app.js
@@ -1,7 +1,7 @@
-var request = require('request');
+var request = require('request'); // use of request method
 request(
     {
-    url: 'https://api.darksky.nett/forecast/8b9bd9588bdbb550822c22490a3db9d3/37.8267,-122.4233',
+    url: 'https://api.darksky.net/forecast/8b9bd9588bdbb550822c22490a3db9d3/37.8267,-122.4233',
     json:true
 },(error,response,body)=> {
  console.log(error);
